8815cc3405ce06d429ffb3fa22e22c59194e4107
Gaby Lau <gabylau3@gmail.com>
2019-05-23 17:20:47 +0000

    finished  sizeof

--- cdemo/mysizeofvariables.c
+++ cdemo/mysizeofvariables.c
@@ -3,6 +3,14 @@
 int main()
 
 {
-int a = 10;
-printf("Var a has a size of %d bytes\n", sizeof(a));
+unsigned int a = 10;
+unsigned char b = 'x';
+float c = 1234.1;
+double d = 12341.1;
+
+printf("int is %d, char is %c, float is %f, double is %lf\n", a, b, c, d);
+printf("Int a has a size of %d bytes\n",  sizeof(a));
+printf("char b has a size of %d bytes\n", sizeof(b));
+printf("float c has a size of %d bytes\n", sizeof(c));
+printf("and double d has a size of %d bytes.\n",  sizeof(d));
 }
