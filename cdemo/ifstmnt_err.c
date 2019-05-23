dc942a1402427414952200ad3d3a43cff4adc805
Gaby Lau <gabylau3@gmail.com>
2019-05-23 19:19:42 +0000

    actual ifstatement commit

---
+++ cdemo/ifstmnt_err.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+int b = 1;
+int c = 2;
+int d = 3;
+
+  // if statement ERROR to test is a is equal to 0
+  if (a == 0)
+  {
+    printf("a is 0\n");
+  }
+  else
+  {
+    printf("a is not 0\n");
+  }
+  if (b != 11)
+{
+	printf("yay you did it\n");
+}  
+if (c > 0)
+{
+	printf("you did it again\n");
+}
+
+if (a == 0 && b == 1)
+{
+	printf("wow you are so smart\n");
+}
+
+}
