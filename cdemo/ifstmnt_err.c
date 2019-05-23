865caad2d05d4c7da7ecc91bbf69de562492bc33
Gaby Lau <gabylau3@gmail.com>
2019-05-23 19:44:24 +0000

    finally finished ifstmt

--- cdemo/ifstmnt_err.c
+++ cdemo/ifstmnt_err.c
@@ -4,30 +4,46 @@ int main()
 {
   int a = 0;
 int b = 1;
-int c = 2;
+int c = 0;
 int d = 3;
 
+printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
   // if statement ERROR to test is a is equal to 0
-  if (a == 0)
+  if (a == c)
   {
-    printf("a is 0\n");
+    printf("a is equal to c\n");
   }
   else
   {
     printf("a is not 0\n");
   }
-  if (b != 11)
+  if (b != a)
 {
-	printf("yay you did it\n");
+	printf("b is not equal to a\n");
 }  
-if (c > 0)
+if (b > c)
 {
-	printf("you did it again\n");
+	printf("b is greater than c\n");
+}
+
+if (d >= c)
+{
+	printf("d is greater than or equal to c\n");
 }
 
 if (a == 0 && b == 1)
 {
-	printf("wow you are so smart\n");
+	printf("a is equal to zero and b is equal to 1\n");
 }
 
+if (a == 0 || b == 0)
+{
+	printf("a is equal to 0, but b is not\n");
+}
+
+if (!(a == 65))
+{
+	printf("a is not equal to 65\n");
+}
 }
