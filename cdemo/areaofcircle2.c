f2e8a463354fec8125257c50a5b933edd1a20853
gabylau3 <42782695+gabylau3@users.noreply.github.com>
2019-05-29 18:29:20 -0700

    Update areaofcircle2.c

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -16,14 +16,23 @@ int main(int argc, char* argv[])
   int higher;
 
   printf("Choose the lower number for the radius range\n"); 
-  fgets(input, 200, stdin);
-  sscanf(input, "%d", &lower);
-  
-  printf("Choose the higher number for the radius range\n");
-  fgets(input, 200, stdin);
-  sscanf(input, "%d", &higher);
 
-;
+  while (1)
+  {
+    fgets(input, 200, stdin);
+    if (sscanf(input, "%d", &lower) == 1) break;
+  printf("Not a valid radius - try again!\n");
+  
+  }
+   printf("Choose the higher number for the radius range\n");
+
+  while (1)
+  {
+    fgets(input, 200, stdin);
+    if (sscanf(input, "%d", &higher) == 1) break;
+  printf("Not a valid radius - try again!\n");
+  
+  }
 
   for (float r = lower; r <= higher; r++ )
   {
