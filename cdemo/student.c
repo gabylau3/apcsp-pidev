9264710c69ed09840cf6241ea4bcfe0be779e105
gabylau3 <42782695+gabylau3@users.noreply.github.com>
2019-05-30 08:35:03 -0700

    Update student.c

--- cdemo/student.c
+++ cdemo/student.c
@@ -14,22 +14,52 @@ void printStudent (struct Student *student)
   printf("First name: %s\n", student->firstname);
   printf("Last name: %s\n", student->lastname);
   printf("Age: %d\n", student->age);
+  printf("Student ID: %d\n", student->studentid);
   }
 
 int main ()
 {
- struct Student Astudent;
- struct Student *student;
- *student = Astudent;
+ struct Student studentArr[50];
+  
+  int studentnumber;
+  char input [300];
+  printf("How many students"?); 
+  fgets(input, 300, stdin);
+  sscanf(input, "%d", &studentnumber);
+  
+  int count = 0;
+  while (count < studentnumber)
+  {
+    char first[20];
+    printf("Enter first name"\n);
+    fgets(input, 300, stdin);
+    sscanf(input, "%s", first);
+    strcpy(studentArr[count].firstname, first);
+    
+    char last[20];
+    printf("Enter last name"\n);
+    fgets(input, 300, stdin);
+    sscanf(input, "%s", last);
+    strcpy(studentArr[count].lastname, first);
+    
+    int ageInput;
+    printf("Enter student age:");
+    fgets(input, 300, stdin);
+    sscanf(input, "%d", &ageInput);
+    studentArr[count].age = ageInput; 
 
- char strA[] = 
- {
-   strcpy(Astudent.firstname, "Abby")};
-   strcpy(Astudent.lastname, "Addams");
-   Astudent.age = 16;
-   Astudent.studentid = 1111111;
+    int idinput;
+    printf("Enter student id:");
+    fgets(input, 300, stdin);
+    sscanf(input, "%d", &idinput); 
+    studentArr[count].studentid = idinput;
+    
+    count++;
+  }
 
-
-printStudent(Astudent);
-
+for (int p = 0; p<studentnumber; p++)
+{
+  printStudent(&studentArr[f]);
+}
+  return 0;
 }
